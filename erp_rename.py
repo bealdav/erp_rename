44240dd461f49f37c7e3c789c96b0f258d1cf6c8
David Beal <david.beal@akretion.com>
2016-09-02 09:31:30 +0200

    [FIX] add missing query and update help fct

--- erp_rename.py
+++ erp_rename.py
@@ -4,9 +4,11 @@
 import sys
 
 
-def help():
+def help_me():
     print """
 This script takes an argument like this example:
+    "oldname1:new1"
+    Or for several renames:
     "oldname1:new1,old2:new2"
 """
 
@@ -14,22 +16,22 @@ QUERIES = [
     "UPDATE ir_model_data SET module = '%s' WHERE module = '%s';",
     "UPDATE ir_module_module SET name = '%s' WHERE name = '%s';",
     "UPDATE ir_module_module_dependency SET name = '%s' WHERE name = '%s';",
+    "UPDATE ir_model_data SET name = 'module_%s' WHERE name = 'module_%s';"
 ]
 
 
 def process():
     if len(sys.argv) == 2:
         modules = sys.argv[1].split(',')
-        mapping = {x[:x.find(':')]: x[x.find(':')+1:]
+        mapping = {x[:x.find(':')]: x[x.find(':') + 1:]
                    for x in modules if ':' in x}
         if not mapping:
-            help()
-            return True
+            return help_me()
         for old, new in mapping.items():
             for query in QUERIES:
                 print query % (new, old)
     else:
-        help()
+        return help_me()
 
 
 if __name__ == '__main__':
